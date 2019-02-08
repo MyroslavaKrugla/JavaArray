d7b139c1f6b7e34a0ff4be83ae3a851a77fd73b4
Myroslava <myroslavs.krugla@gmail.com>
2019-02-08 14:37:19 +0300

    finding second minimum value from array

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //int arr[]= new int [5];
+        int arr []= {-5, 7, 12,0,-3};
+        for (int i = 0; i < arr.length; i++){
+            int min = arr[i];
+            int min_i = i;
+            for (int j = i +1; j < arr.length; j++){
+                if (arr[j] < min) {
+                    min = arr[j];
+                    min_i = j;
+                }
+            }
+            if (i != min_i) {
+                int tmp = arr[i];
+                arr[i] = arr[min_i];
+                arr[min_i] = tmp;
+            }
+        }
+        System.out.println("Второй минимальный элемент массива: " + arr[1]);
+    }
+}
